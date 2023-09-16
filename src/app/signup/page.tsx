91cb17c0f56ac5bd398d854fab911e7783e542fa
AlgoAngel <laoroney1113@gmail.com>
2023-09-16 15:38:00 +0530

    Feat - Signup page created

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,109 @@
-export default function SignUp() {
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router"; // Changed from "next/navigation"
+import axios from "axios"; // Removed the curly braces around axios
+
+const SignUp = () => {
+  // Initialize state variables to store form input values
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  // Event handler to update the state when input values change
+  const handleInputChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  // Event handler to log the form input values when the form is submitted
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log("Form values:", user);
+  };
+
   return (
-    <section className="container p-6 text-center text-2xl">
-      <h1>Sign Up Page</h1>
+    <section className="container p-6 text-center text-2xl flex justify-center items-center h-[100vh]">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Create an account
+          </h1>
+          <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="username"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                User Name
+              </label>
+              <input
+                type="text"
+                name="username"
+                placeholder="UserName"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                value={user.username}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                value={user.email}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="confirm-password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                value={user.password}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Create an account
+            </button>
+            <Link
+              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              href="/login"
+            >
+              Go to Login Page
+            </Link>
+          </form>
+        </div>
+      </div>
     </section>
   );
-}
+};
+
+export default SignUp;

1cc3a2ca3955631ac321ae437593ee5c57906f09
AlgoAngel <laoroney1113@gmail.com>
2023-09-16 14:22:31 +0530

    Feat - Page Created

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <section className="container p-6 text-center text-2xl">
+      <h1>Login Page</h1>
+    </section>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <section className="container p-6 text-center text-2xl">
+      <h1>Sign Up Page</h1>
+    </section>
+  );
+}

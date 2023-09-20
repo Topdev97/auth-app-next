38790d4ad959e1ca4f342fe1259a8d1a7e772601
AlgoAngel <laoroney1113@gmail.com>
2023-09-20 12:16:33 +0530

    Fix - timestamp added in schema

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,33 +1,38 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Please provide a username"],
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide a email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    isVerfied: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
   },
-  email: {
-    type: String,
-    required: [true, "Please provide a email"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-  },
-  isVerfied: {
-    type: Boolean,
-    default: false,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  forgotPasswordToken: String,
-  forgotPasswordTokenExpiry: Date,
-  verifyToken: String,
-  verifyTokenExpiry: Date,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.models.users || mongoose.model("users", userSchema);
 
